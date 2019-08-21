ac9157720ed566cf440fd46f64f533f9fa33cf7d
Samuell <Samuell1@users.noreply.github.com>
2019-08-21 20:06:36 +0200

    Boot files changes

--- index.php
+++ index.php
@@ -1,17 +1,23 @@
 <?php
+
 /**
- * October - The PHP platform that gets back to basics.
+ * Laravel - A PHP Framework For Web Artisans
  *
- * @package  October
- * @author   Alexey Bobkov, Samuel Georges
+ * @package  Laravel
+ * @author   Taylor Otwell <taylor@laravel.com>
  */
+
+define('LARAVEL_START', microtime(true));
 
 /*
 |--------------------------------------------------------------------------
-| Register composer
+| Register The Auto Loader
 |--------------------------------------------------------------------------
 |
-| Composer provides a generated class loader for the application.
+| Composer provides a convenient, automatically generated class loader for
+| our application. We just need to utilize it! We'll simply require it
+| into the script here so that we don't have to worry about manual
+| loading any of our classes later on. It feels great to relax.
 |
 */
 
@@ -19,10 +25,13 @@ require __DIR__.'/bootstrap/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
-| Load framework
+| Turn On The Lights
 |--------------------------------------------------------------------------
 |
-| This bootstraps the framework and loads up this application.
+| We need to illuminate PHP development, so let us turn on the lights.
+| This bootstraps the framework and gets it ready for use, then it
+| will load up this application so that we can run it and send
+| the responses back to the browser and delight our users.
 |
 */
 
@@ -30,14 +39,17 @@ $app = require_once __DIR__.'/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
-| Process request
+| Run The Application
 |--------------------------------------------------------------------------
 |
-| Execute the request and send the response back to the client.
+| Once we have the application, we can handle the incoming request
+| through the kernel, and send the associated response back to
+| the client's browser allowing them to enjoy the creative
+| and wonderful application we have prepared for them.
 |
 */
 
-$kernel = $app->make('Illuminate\Contracts\Http\Kernel');
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $response = $kernel->handle(
     $request = Illuminate\Http\Request::capture()

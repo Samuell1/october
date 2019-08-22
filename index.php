b4ba34897f8c01f667c7e5680c1767ab24a8a4be
Samuell <Samuell1@users.noreply.github.com>
2019-08-22 19:21:18 +0200

    Revert index comment

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
 /**
+ * October - The PHP platform that gets back to basics.
  * Laravel - A PHP Framework For Web Artisans
  *
- * @package  Laravel
- * @author   Taylor Otwell <taylor@laravel.com>
+ *
+ * @package  October
+ * @author   Alexey Bobkov, Samuel Georges
  */
 
 define('LARAVEL_START', microtime(true));
